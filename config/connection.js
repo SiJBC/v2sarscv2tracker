5489f31bfe0d5648392965d816ee210c525110cb
Simon Colman <s_colman@hotmail.com>
2020-03-21 17:56:57 +1100

    heroku

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection; 
+
+console.log("Look Here")
+if (process.env.JAWSDB_URL){
+  console.log("In JAWSDB_URL")
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else{
+  console.log("NOOOOT In JAWSDB_URL")
+ connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "exampledb, testdb"
+  });
+}
+
+
+// Make connection.
+connection.connect()
+
+// Export connection for our ORM to use.
+module.exports = connection;
